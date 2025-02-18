d38f0211ca7ba248f52c93f00801423a6fc6bf8f
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-19 00:56:41 +0300

    feat: setting up @nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const ENV = process.env.NODE_ENV || 'development';
+
+  const config = new DocumentBuilder()
+    .setTitle('tour management system backend api')
+    .setDescription('The tour management system backend api built using NestJs')
+    .setVersion('1.0.0')
+    .addServer(
+      ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://tour-management-system.render.com',
+    )
+    .addBearerAuth() // Add Bearer token support
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
