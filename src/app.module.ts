048753e83faf55c1deaaf66f376e6585692f1611
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-18 23:55:11 +0300

    feat: basic setup | generate modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { ToursModule } from './tours/tours.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    BookingsModule,
+    ToursModule,
+    UsersModule,
+    ReviewsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
