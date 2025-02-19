8eacaa8da0d4e734132462048c91fa3b9fbdb354
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-19 09:58:39 +0300

    feat: connecting nestjs with pg using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -14,6 +15,19 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     ReviewsModule,
     AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'postgres',
+      database: process.env.DATABASE_NAME || 'tour_management_db',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: Boolean(process.env.NODE_ENV === 'production'),
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
