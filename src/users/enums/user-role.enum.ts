22e083eb3d54c54a8549b717575482c181fa19db
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-19 22:30:08 +0300

    feat: add User entity and UserRole enum

---
+++ src/users/enums/user-role.enum.ts
@@ -0,0 +1,6 @@
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+  GUIDE = 'guide',
+  LEAD_GUIDE = 'lead-guide',
+}
---
+++ src/users/user.entity.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from './enums/user-role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    nullable: true,
+  })
+  profilePicture?: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: string;
+}
