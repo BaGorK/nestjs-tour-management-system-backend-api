a8d3d369a87fcb37961ccd71a16c0b12f03c1a51
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-19 10:55:36 +0300

    feat: integrate configuration management with @nestjs/config and add environment validation

---
+++ src/lib/config/database.config.ts
@@ -0,0 +1,12 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  port: Number(process.env.DATABASE_PORT || 5432),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST || 'localhost',
+  database_name: process.env.DATABASE_NAME,
+  autoLoadEntities: Boolean(process.env.NODE_ENV === 'development'),
+  synchronize: Boolean(process.env.NODE_ENV === 'development'),
+  rejectUnauthorized: Boolean(process.env.NODE_ENV === 'production'),
+}));
