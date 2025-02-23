7f44c94282d922520e785ff6d09495bbf982ff2d
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-23 20:19:40 +0300

    master:feat: adding jwt configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import jwtConfig from './config/jwt.config';
 import { AuthService } from './providers/auth.service';
-import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
+import { HashingProvider } from './providers/hashing.provider';
 import { SignInProvider } from './providers/sign-in.provider';
 import { SignUpProvider } from './providers/sign-up.provider';
 
@@ -18,5 +22,10 @@ import { SignUpProvider } from './providers/sign-up.provider';
     SignUpProvider,
   ],
   exports: [AuthService, HashingProvider],
+  imports: [
+    UsersModule,
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
 })
 export class AuthModule {}
