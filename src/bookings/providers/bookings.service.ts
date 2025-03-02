c8fc44ed2a13d989268864e35cff624a39dee7cb
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-02 21:44:48 +0300

    master:feat: create a provider for finding a booking by condition

--- src/bookings/providers/bookings.service.ts
+++ src/bookings/providers/bookings.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Booking } from '../booking.entity';
 import { DeleteBookingProvider } from './delete-booking.provider';
 import { FindBookingByIdProvider } from './find-booking-by-id.provider';
+import { FindOneBookingByProvider } from './find-one-booking-by.provider';
 
 @Injectable()
 export class BookingsService {
   constructor(
     private readonly deleteBookingProvider: DeleteBookingProvider,
     private readonly findBookingByIdProvider: FindBookingByIdProvider,
+    private readonly findOneBookingByProvider: FindOneBookingByProvider,
   ) {}
   // find booking by id
   public findBookingById(id: string) {
@@ -16,5 +19,10 @@ export class BookingsService {
   // delete tour
   public deleteBooking(id: string) {
     return this.deleteBookingProvider.deleteBooking(id);
+  }
+
+  // find booking by
+  public findOneBookingBy(options: Partial<Booking>) {
+    return this.findOneBookingByProvider.findOneBookingBy(options);
   }
 }
---
+++ src/bookings/providers/find-one-booking-by.provider.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Booking } from '../booking.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class FindOneBookingByProvider {
+  constructor(
+    @InjectRepository(Booking)
+    private readonly bookingsRepository: Repository<Booking>,
+  ) {}
+
+  public async findOneBookingBy(options: Partial<Booking>) {
+    console.log('find one booking by...');
+
+    let booking = undefined;
+
+    try {
+      booking = await this.bookingsRepository.findOneBy(options);
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(
+        'Unable to find booking by condition',
+      );
+    }
+
+    if (!booking) {
+      throw new NotFoundException(`Booking not found.`);
+    }
+
+    return booking;
+  }
+}
