f5b42cbe77a0d4a156ec3746fb78261f492bc956
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-02 19:09:52 +0300

    master:import all providers, services and controllers insider tours module

--- src/tours/tours.module.ts
+++ src/tours/tours.module.ts
@@ -1,4 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileUploadModule } from 'src/common/file-upload/file-upload.module';
+import { TourImages } from './entities/tour-images.entity';
+import { Tour } from './entities/tour.entity';
+import { CreateTourProvider } from './providers/create-tour.provider';
+import { DeleteTourProvider } from './providers/delete-tour.provider';
+import { FindOneTourByProvider } from './providers/find-one-tour-by.provider';
+import { GetAllToursProvider } from './providers/get-all-tours.provider';
+import { GetTourByIdProvider } from './providers/get-tour-by-id.provider';
+import { FindMultipleImagesByIdsProvider } from './providers/tour-images/find-multiple-images-by-ids.provider';
+import { ToursService } from './providers/tours.service';
+import { UpdateTourProvider } from './providers/update-tour.provider';
+import { ToursController } from './tours.controller';
 
-@Module({})
+@Module({
+  controllers: [ToursController],
+  providers: [
+    ToursService,
+    GetAllToursProvider,
+    CreateTourProvider,
+    UpdateTourProvider,
+    DeleteTourProvider,
+    GetTourByIdProvider,
+    FindOneTourByProvider,
+    FindMultipleImagesByIdsProvider,
+  ],
+  imports: [TypeOrmModule.forFeature([Tour, TourImages]), FileUploadModule],
+})
 export class ToursModule {}
