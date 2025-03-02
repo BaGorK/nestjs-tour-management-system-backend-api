8989f87fb5da3bf9b4fed8f58ddb0d967ef1e252
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-02 19:07:28 +0300

    master:feat: a new api endpoint for getting all tours

---
+++ src/tours/providers/get-all-tours.provider.ts
@@ -0,0 +1,30 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tour } from '../entities/tour.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class GetAllToursProvider {
+  constructor(
+    @InjectRepository(Tour)
+    private readonly toursRepository: Repository<Tour>,
+  ) {}
+
+  public async getAllTours() {
+    console.log('get all tours...');
+    try {
+      const tours = await this.toursRepository.find();
+
+      return {
+        status: 'success',
+        message: 'get all tours',
+        data: tours,
+      };
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(
+        'Failed to fetch tours. Please try again.',
+      );
+    }
+  }
+}
