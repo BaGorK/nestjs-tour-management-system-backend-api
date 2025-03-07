02e69137748266edfdc12f34e4eadc41e55d47fe
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-07 09:35:15 +0300

    master:staff entity, create staff dto, and migration for staff

---
+++ src/staff/dtos/create-staff.dto.ts
@@ -0,0 +1,64 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { StaffRole } from '../enum/staff-role.enum';
+
+export class CreateStaffDto {
+  @ApiProperty({
+    description: 'First name of the staff member',
+    example: 'John',
+  })
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty({
+    description: 'Last Name of the staff member',
+    example: 'Doe',
+  })
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty({
+    description: 'Last Name of the staff member',
+    example: 'john@doe.com',
+  })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'Last Name of the staff member',
+    example: 'test1234',
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'password should be at least 8 Characters' })
+  @MaxLength(96)
+  password?: string;
+
+  @ApiProperty({
+    description: 'profile images of the member',
+    type: 'string',
+    format: 'binary',
+    required: true,
+  })
+  @IsString()
+  profilePicture?: string;
+
+  @ApiProperty({
+    description: 'role of the staff member',
+    enum: StaffRole,
+    example: StaffRole.GUIDE,
+  })
+  @IsNotEmpty()
+  @IsEnum(StaffRole)
+  role: StaffRole;
+}
--- src/staff/staff.entity.ts
+++ src/staff/staff.entity.ts
@@ -1,7 +1,38 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { StaffRole } from './enum/staff-role.enum';
 
 @Entity()
 export class Staff {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    nullable: true,
+  })
+  @Exclude()
+  password?: string;
+
+  @Column({
+    nullable: true,
+  })
+  profilePicture?: string;
+
+  @Column({
+    type: 'enum',
+    enum: StaffRole,
+    default: StaffRole.GUIDE,
+  })
+  role: StaffRole;
 }
