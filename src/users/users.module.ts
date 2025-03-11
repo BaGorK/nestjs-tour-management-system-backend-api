6fd9dcba03702839cc0b50f795dbdc18baec7abf
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-11 20:23:12 +0300

    master:add file for reset password and update password functionality to be implemented

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,13 +4,19 @@ import { AuthModule } from 'src/auth/auth.module';
 import { CreateUserProvider } from './providers/crud/create-user.provider';
 import { FindAllUsersProvider } from './providers/crud/find-all-users.provider';
 import { UpdateUserProvider } from './providers/crud/update-user.provider';
+import { FindCurrentActiveUserProvider } from './providers/current/find-current-active-user.provider';
+import { ForgotMyPasswordProvider } from './providers/current/forgot-my-password.provider';
+import { ResetMyPasswordProvider } from './providers/current/reset-my-password.provider';
+import { UpdateMeProvider } from './providers/current/update-me.provider';
+import { UpdateMyPasswordProvider } from './providers/current/update-my-password.provider';
 import { FindOneUserByProvider } from './providers/find-one-user-by.provider';
 import { FindOneUserWithBookingsProvider } from './providers/find-one-user-with-bookings.provider';
 import { CreateGoogleUserProvider } from './providers/social/create-google-user.provider';
 import { UsersService } from './providers/users.service';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
-import { FindCurrentActiveUserProvider } from './providers/current/find-current-active-user.provider';
+import { EmailModule } from 'src/common/email/email.module';
+import { TwilioModule } from 'src/common/twilio/twilio.module';
 
 @Module({
   controllers: [UsersController],
@@ -23,8 +29,17 @@ import { FindCurrentActiveUserProvider } from './providers/current/find-current-
     CreateGoogleUserProvider,
     FindOneUserWithBookingsProvider,
     FindCurrentActiveUserProvider,
+    UpdateMyPasswordProvider,
+    ForgotMyPasswordProvider,
+    ResetMyPasswordProvider,
+    UpdateMeProvider,
   ],
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [
+    EmailModule,
+    TwilioModule,
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => AuthModule),
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
