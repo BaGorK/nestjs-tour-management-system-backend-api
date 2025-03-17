2adcbb48a5b05b6ecf63f4ed55b3aaf173eefeff
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-17 13:50:44 +0300

    master:feat: update time out 5 sec to 20 sec

--- src/common/interceptors/timeout/timeout.interceptor.ts
+++ src/common/interceptors/timeout/timeout.interceptor.ts
@@ -17,7 +17,7 @@ import {
 export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      timeout(5000),
+      timeout(20000),
       catchError(err => {
         if (err instanceof TimeoutError) {
           return throwError(() => new RequestTimeoutException());
