994edbebc1aeeb1ffc5f522d84e793a20f816956
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-03-17 12:52:06 +0300

    master:feat: find staff by id with tours detail

--- src/staff/providers/crud/find-staff-by-id.provider.ts
+++ src/staff/providers/crud/find-staff-by-id.provider.ts
@@ -1,16 +1,37 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { FindOneStaffByProvider } from '../find-one-staff-by.provider';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Staff } from 'src/staff/staff.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class FindStaffByIdProvider {
   constructor(
-    private readonly findOneStaffByProvider: FindOneStaffByProvider,
+    @InjectRepository(Staff)
+    private readonly staffRepository: Repository<Staff>,
   ) {}
 
   public async findStaffById(id: string) {
     console.log('find staff by id provider...');
 
-    const staff = await this.findOneStaffByProvider.findOneStaffBy({ id });
+    let staff: Staff | undefined;
+
+    try {
+      staff = await this.staffRepository.findOne({
+        where: { id },
+        relations: {
+          tours: true,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(
+        'Unable to find staff by id, please try again later.',
+      );
+    }
 
     if (!staff) {
       throw new NotFoundException(`Staff with ID=${id} not found.`);
